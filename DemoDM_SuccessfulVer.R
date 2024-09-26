e976bbbc8769a00899da2b1f20ee516e43b0b67f
mihoyoMhb <menghangb_ba@outlook.com>
2024-09-26 22:55:26 +0200

    Update DemoDM_SuccessfulVer.R

--- DemoDM_SuccessfulVer.R
+++ DemoDM_SuccessfulVer.R
@@ -62,7 +62,7 @@ manualDM=function(roads,car,packages) {
 averageTest <- function(tests){
   sum = 0
   for (i in 1:tests) {
-    sum=sum+runDeliveryMan(aStarSearchDM, dim = 10, turns = 2000, doPlot = F, pause = 0, del = 5)
+    sum=sum+runDeliveryMan(A_SearchDM, dim = 10, turns = 2000, doPlot = F, pause = 0, del = 5)
     if(i%%10==0){
       print(i)
       print(sum/i)
